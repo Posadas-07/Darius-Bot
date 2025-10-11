af6b17259af48f61c59a4a47797af47dd393f241
Posadas-07 <elderposadas028@gmail.com>
2025-10-11 14:36:34 -0600

    Actualizar Menu.js

--- plugins/pluginsgrupos/Menu.js
+++ plugins/pluginsgrupos/Menu.js
@@ -32,7 +32,7 @@ const handler = async (msg, { conn }) => {
     console.error("[menu] Error leyendo setmenu.json:", e);
   }
 
-  const caption = `╔══  ═══
+  const caption = `╔══ xtayrusx ═══
 ║𝙈𝙀𝙉𝙐 𝙂𝙀𝙉𝙀𝙍𝘼𝙇
 ║𝗣𝗿𝗲𝗳𝗶𝗷𝗼 𝗔𝗰𝘁𝘂𝗮𝗹: 『 ${pref} 』
 ║𝗨𝘀𝗮 𝗲𝗻 𝗰𝗮𝗱𝗮 𝗰𝗼𝗺𝗮𝗻𝗱𝗼
